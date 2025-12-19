acfbd84daf794f76f0af3231360e2b03d0e73654
nyfhemi2018-ai <nyfhemi2018@gmail.com>
2025-12-19 12:20:07 +0100

    Update site

--- js/main.js
+++ js/main.js
@@ -1,55 +1,40 @@
 (function ($) {
     "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
-    // Initiate the wowjs
+    /* Spinner */
+    setTimeout(function () {
+        if ($('#spinner').length) {
+            $('#spinner').removeClass('show');
+        }
+    }, 1);
+
+    /* WOW animation */
     new WOW().init();
 
-
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 0) {
-            $('.navbar').addClass('position-fixed bg-dark shadow-sm');
+    /* Sticky Navbar */
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 50) {
+            $('.navbar').addClass('scrolled');
         } else {
-            $('.navbar').removeClass('position-fixed bg-dark shadow-sm');
+            $('.navbar').removeClass('scrolled');
         }
     });
-    window.addEventListener('scroll', function () {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
-});
 
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
+    /* Back to top button */
+    $(window).on('scroll', function () {
         if ($(this).scrollTop() > 300) {
             $('.back-to-top').fadeIn('slow');
         } else {
             $('.back-to-top').fadeOut('slow');
         }
     });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+
+    $('.back-to-top').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
 
-
-    // Testimonials carousel
+    /* Testimonials carousel */
     $('.testimonial-carousel').owlCarousel({
         autoplay: true,
         smartSpeed: 1000,
@@ -57,19 +42,17 @@
         nav: false,
         dots: true,
         items: 1,
-        dotsData: true,
+        dotsData: true
     });
 
-    <script>
-document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
-    link.addEventListener('click', () => {
-        const navbar = document.querySelector('.navbar-collapse');
-        if (navbar.classList.contains('show')) {
-            new bootstrap.Collapse(navbar).toggle();
-        }
+    /* Close mobile navbar on click */
+    document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
+        link.addEventListener('click', () => {
+            const navbar = document.querySelector('.navbar-collapse');
+            if (navbar && navbar.classList.contains('show')) {
+                new bootstrap.Collapse(navbar).hide();
+            }
+        });
     });
-});
-</script>
 
 })(jQuery);
-
